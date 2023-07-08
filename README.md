49635ab5f05fe64a0251f9115d2c7e4064696df2
evgenii.saurov <evgenii.saurov@supperundsupper.com>
2023-07-09 01:04:59 +0200

    fix countdown block counter

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
+# Guess the number game APP
+
+This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+
 ## Available Scripts
 
 ### Before run the project
--- src/features/CountDownTimer/ui/index.tsx
+++ src/features/CountDownTimer/ui/index.tsx
@@ -1,37 +1,14 @@
-import React, { useEffect, useState } from "react";
-
-export const CountdownTimer = ({ blocks }: { blocks: number }) => {
-  const [countDown, setCountDown] = useState(blocks * 12 * 1000);
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setCountDown((prevCountDown) => prevCountDown - 1000); // Уменьшаем таймер на 1 секунду (1000 миллисекунд)
-
-      if (countDown <= 0) {
-        clearInterval(interval);
-        setCountDown(0);
-      }
-    }, 1000);
-
-    return () => clearInterval(interval);
-  }, []);
-
-  useEffect(() => {
-    setCountDown(blocks * 12 * 1000);
-  }, [blocks]);
-
-  const formatTime = (time: number) => {
-    const minutes = Math.floor(time / (1000 * 60));
-    const seconds = Math.floor((time / 1000) % 60);
-    return `${minutes}м ${seconds}с`;
-  };
-
-  return countDown > 0 ? (
+export const CountdownTimer = ({ blocks }: { blocks: number }) =>
+  blocks > 0 ? (
     <div>
       <h2>Countdown timer</h2>
-      <div>{formatTime(countDown)}</div>
+      <div className="relative">
+        <span className="text-2xl font-bold animate-ping opacity-100">
+          {blocks}{" "}
+        </span>
+        {blocks > 1 ? "blocks are" : "block is"} remaining
+      </div>
     </div>
   ) : null;
-};
 
 export default CountdownTimer;
